a03f08f3b92c4c453c265aaef4293cf40e5e6fca
alexvieru1 <vieru.alexandruu@gmail.com>
2024-08-09 09:12:08 +0300

    hero change

--- components/page-sections/HomePage.tsx
+++ components/page-sections/HomePage.tsx
@@ -1,17 +1,30 @@
 "use client";
 import React from "react";
-import { WavyBackground } from "../ui/wavy-background";
 import { motion } from "framer-motion";
 import { AnimatedBall } from "../ui/animated-ball";
+import { BackgroundGradientAnimation } from "../ui/background-gradient-animation";
 
 const title = "AV";
 const subtitle = "Crafting Digital Experiences";
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <WavyBackground className="max-w-5xl mx-auto pb-40">
-        <div className="flex flex-col justify-center items-center select-none mt-36">
+    <div className="relative flex items-center justify-center min-h-screen">
+      <BackgroundGradientAnimation
+        gradientBackgroundStart="rgb(0, 0, 0)" // Starting color of the background gradient
+        gradientBackgroundEnd="rgb(0, 0, 0)" // Ending color of the background gradient
+        firstColor="0, 153, 255" // Blue color
+        secondColor="221, 74, 255" // Purple color
+        thirdColor="100, 220, 255" // Cyan color
+        fourthColor="200, 50, 50" // Red color
+        fifthColor="180, 180, 50" // Yellow color
+        pointerColor="140, 100, 255" // Light purple for the pointer
+        size="100%" // Size of the animated elements
+        blendingValue="hard-light" // Blending mode for the animated elements
+        interactive={true} // Allow interaction
+        className="absolute inset-0 w-full h-full" // Full-screen background animation
+      >
+        <div className="relative z-10 flex flex-col justify-center items-center select-none mt-36 lg:mt-10">
           <motion.div
             initial="hidden"
             animate="visible"
@@ -29,7 +42,7 @@ const HomePage = () => {
               <motion.span
                 key={index}
                 variants={{
-                  hidden: { opacity: 0, y: index % 2 === 0 ? -100 : 100 },
+                  hidden: { opacity: 0, y: index % 2 === 0 ? 100 : -100 },
                   visible: { opacity: 1, y: 0 },
                 }}
                 transition={{ duration: 2 }} // Duration of 2 seconds for each letter animation
@@ -50,24 +63,24 @@ const HomePage = () => {
               hidden: {},
               visible: {
                 transition: {
-                  staggerChildren: 0.1, // Staggering each letter by 0.1 second for a shorter animation
+                  staggerChildren: 0.05, // Staggering each letter by 0.05 second for a quicker animation
                   delayChildren: 2.5, // Delay before starting the animation for "Crafting Digital Experiences"
                 },
               },
             }}
             className="flex flex-wrap items-center justify-center text-white font-semibold pointer-events-none text-4xl text-center md:text-5xl lg:text-5xl"
           >
-            {subtitle.split(" ").map((word, index) => (
+            {subtitle.split("").map((letter, index) => (
               <motion.span
                 key={index}
                 variants={{
                   hidden: { opacity: 0, y: 100 },
                   visible: { opacity: 1, y: 0 },
                 }}
-                transition={{ duration: 0.5 }} // Duration of 0.5 seconds for each word animation
-                className="block w-full md:w-auto md:inline-block"
+                transition={{ duration: 0.5 }} // Duration of 0.5 seconds for each letter animation
+                className={letter === " " ? "inline-block w-2" : ""}
               >
-                {word}&nbsp;
+                {letter === " " ? "\u00A0" : letter}
               </motion.span>
             ))}
           </motion.div>
@@ -75,7 +88,7 @@ const HomePage = () => {
             <AnimatedBall />
           </div>
         </div>
-      </WavyBackground>
+      </BackgroundGradientAnimation>
     </div>
   );
 };
